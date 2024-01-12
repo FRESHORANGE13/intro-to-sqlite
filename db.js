c3aaf1cecf644f6b71907a67b912b277998f27f1
Alisa1098 <118067084+Alisa1098@users.noreply.github.com>
2024-01-12 12:58:10 -0500

    stillhavequestions

--- db.js
+++ db.js
@@ -20,7 +20,9 @@ const db = new sqlite.Database(dbFile, (error) => {
 const getUserById = (request, response) => {
   // Parse the id to generate a SQLite query
   const id = parseInt(request.params.id);
+  //runs this sequel statement whe called: 
   const query = `SELECT * FROM user WHERE id = ?`;
+  console.log(query);
 
   // db.get will replace all ? in query sequentially with
   // items from the array passed as the second parameter
@@ -44,9 +46,109 @@ const getUserById = (request, response) => {
 // ----- FILL IN BELOW -----
 // Write and export the rest of the functions needed by index.js!
 
+//get all 
+const getAllUsers = (request, response) => {
+  const query = `SELECT * FROM user`;
+
+  db.all("SELECT * FROM user", (err, result)=> {
+    console.log(result);
+    if (error) {
+      console.error(error.message);
+      response.status(400).json({ error: error.message });
+      return;
+    }
+    if (result) {
+      response.json(result);
+    } else {
+      response.sendStatus(404);
+    }
+  });
+};
+
+//insert 
+const insertUser = (request, response) => {
+  const name = request.params.name;
+  const id = parseInt(request.params.id);
+  const query = "INSERT INTO users VALUES (?, ?)";
+
+  //id first and then names so thats the order in array
+  db.insert (query, [id,name], (err, result) => {
+    
+    if (error) {
+      console.error(error.message);
+      response.status(400).json({ error: error.message });
+      return;
+    }
+    // If nothing is returned, then result will be undefined
+    if (result) {
+      response.json(result);
+    } else {
+      response.sendStatus(404);
+    }
+})
+
+}
+
+//update name: 
+const updateName = (request, response) => {
+  const id = parseInt(request.params.id);
+  const name = request.params.name;
+  //check if this query is right 
+  const query = "UPDATE users SET name = ? WHERE id = ?";
+
+  db.run(query, [id,name],(error, result) => {
+    if (error) {
+      console.error(error.message);
+      response.status(400).json({ error: error.message });
+      return;
+    }
+    // If nothing is returned, then result will be undefined
+    if (result) {
+      response.json(result);
+    } else {
+      response.sendStatus(404);
+    }
+  } );
+}
+
+//delete by id
+const del = (request, result) => {
+  const id = parseInt(request.params.id);
+  const query = "DELTE FROM users WHERE id = ?";
+
+  db.run(query, [id], (error, result) => {
+    if (error) {
+      console.error(error.message);
+      response.status(400).json({ error: error.message });
+      return;
+    }
+    // If nothing is returned, then result will be undefined
+    if (result) {
+      response.json(result);
+    } else {
+      response.sendStatus(404);
+    }
+  })
+}
+
+
+
 //#endregion Routes
 
 // This allows `index.js` to use functions defined in this file.
 module.exports = {
   getUserById,
+};
+
+module.exports = {
+  getAllUsers,
+};
+module.exports = {
+  insertUser,
+};
+module.exports = {
+  updateName,
+};
+module.exports = {
+  del,
 };
--- index.js
+++ index.js
@@ -44,18 +44,23 @@ app.get("/", (req, res) => {
 
 // This endpoint allows a client to get a single user by id
 app.get("/user/:id", db.getUserById);
+//id (whats after the :) is a variable name of sorts
 
 // ------ FILL IN BELOW -------
 // Write endpoints that allow a client to:
 
 // Get all users
+app.get("/users", db.getAllUsers);
 
 // Create a new user
 
+app.get("/users/:name/:id", db.insertUser);
+
 // Update a user's name, given an id
+app.get("/users/:id/:name", db.updateName)
 
 // Delete a user by id
-
+app.get("/users/:id", db.del);
 //#endregion Database Routes
 
 //-----------------------------
